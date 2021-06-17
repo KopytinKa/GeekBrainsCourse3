3cae8d64152cd0120800c6827d83d7e181cb1978
KIrill Kopytin <kopytin-ka@mail.ru>
2021-06-17 19:20:03 +0300

    Выполнены задания: 1) добавлен синглтон для хранения данных о сессии

---
+++ VKClient/Model/Session.swift
@@ -0,0 +1,17 @@
+//
+//  Session.swift
+//  VKClient
+//
+//  Created by Кирилл Копытин on 17.06.2021.
+//
+
+import Foundation
+
+final class Session {
+    static let shared = Session()
+    
+    private init() {}
+    
+    var token: String = ""
+    var userId: Int = 0
+}
